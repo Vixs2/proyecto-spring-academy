ef84169f3342f6685cfd6ec63f3c6e798a16ac75
Vixs2 <santiago.sr5897@gmail.com>
2026-01-07 16:40:58 -0500

    Cambios agregados, con error de archivo no encontrado

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -1,6 +1,5 @@
 package com.example.cashcard;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +11,6 @@ import java.util.Optional; // ¡Asegúrate de agregar este import!
 @RequestMapping("/cashcards")
 public class CashCardController {
    
-   @Autowired
    private final CashCardRepository cashCardRepository;
 
    // El constructor inyecta el repositorio correctamente
---
+++ src/main/java/com/example/cashcard/JdbcConfiguration.java
@@ -0,0 +1,8 @@
+package com.example.cashcard;
+
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+
+@EnableJdbcRepositories
+public class JdbcConfiguration {
+    
+}
